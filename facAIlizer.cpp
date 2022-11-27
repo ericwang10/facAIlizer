4eaaa236889d99687e443ab9af25d5ce08584f32
Eric Wang <e.wang192@gmail.com>
2022-11-27 17:24:40 -0500

    Add project files.

---
+++ facAIlizer.cpp
@@ -0,0 +1,100 @@
+﻿// facAIlizer.cpp : Defines the entry point for the application.
+//
+
+#include "facAIlizer.h"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+#include <Eigen/Dense>
+#define FDEEP_FLOAT_TYPE double
+
+#include <fdeep/fdeep.hpp>
+
+using Eigen::MatrixXd;
+
+
+using namespace std;
+using namespace cv;
+void detectAndDisplay(Mat frame);
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+int main(int argc, const char** argv)
+{
+    const auto model = fdeep::load_model("C:/Users/Pinkie/source/repos/facAIlizer/fdeep_model.json", true);
+
+
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    std::cout << m << std::endl;
+
+    String face_cascade_name = "C:/Users/Pinkie/source/repos/facAIlizer/haarcascade_eye_tree_eyeglasses.xml";
+    String eyes_cascade_name = "C:/Users/Pinkie/source/repos/facAIlizer/haarcascade_frontalface_alt.xml";
+    //-- 1. Load the cascades
+    if (!face_cascade.load(face_cascade_name))
+    {
+        cout << "--(!)Error loading face cascade\n";
+        return -1;
+    };
+    if (!eyes_cascade.load(eyes_cascade_name))
+    {
+        cout << "--(!)Error loading eyes cascade\n";
+        return -1;
+    };
+    VideoCapture capture;
+    //-- 2. Read the video stream
+    capture.open(0);
+    if (!capture.isOpened())
+    {
+        cout << "--(!)Error opening video capture\n";
+        return -1;
+    }
+    Mat frame;
+    while (capture.read(frame))
+    {
+        if (frame.empty())
+        {
+            cout << "--(!) No captured frame -- Break!\n";
+            break;
+        }
+        //-- 3. Apply the classifier to the frame
+        detectAndDisplay(frame);
+        if (waitKey(10) == 27)
+        {
+            break; // escape
+        }
+    }
+    return 0;
+}
+void detectAndDisplay(Mat frame)
+{
+    Mat frame_gray;
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+    equalizeHist(frame_gray, frame_gray);
+    //-- Detect faces
+    std::vector<Rect> faces;
+    face_cascade.detectMultiScale(frame_gray, faces);
+    for (size_t i = 0; i < faces.size(); i++)
+    {
+        Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
+        ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
+        Mat faceROI = frame_gray(faces[i]);
+        //-- In each face, detect eyes
+        std::vector<Rect> eyes;
+        eyes_cascade.detectMultiScale(faceROI, eyes);
+        for (size_t j = 0; j < eyes.size(); j++)
+        {
+            Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
+            int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
+            circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
+        }
+    }
+    //-- Show what you got
+    imshow("Capture - Face detection", frame);
+}
