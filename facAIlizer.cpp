01e8c9951b381b744a1cf0d6a07974d3a9714c41
Jessica Yang <jessicayangg01@gmail.com>
2022-11-28 02:36:56 -0500

    just testing commit

--- facAIlizer.cpp
+++ facAIlizer.cpp
@@ -2,14 +2,7 @@
 //
 
 #include "facAIlizer.h"
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
-#include <opencv2/opencv.hpp>
 
-#include <iostream>
-#include <fdeep/fdeep.hpp>
 
 
 
